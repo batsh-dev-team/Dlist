aa5450a2bb145d338c3c1faef7aa9e4d2de02e38
BYVoid <byvoid@byvoid.com>
2013-10-02 16:43:07 +0100

    Add documentation.

--- dlist.mli
+++ dlist.mli
@@ -25,34 +25,93 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *)
 
+(** Difference list: a list-like data structure with O(1) concatenation. *)
+(** Every method listed below is {b tail recursive}, so you can use them safely.
+The APIs are inspaired by {{: https://ocaml.janestreet.com/ocaml-core/latest/doc/core_kernel/Core_list.html}
+Core.List}.
+*)
+
+(** The abstract type of Dlist. *)
 type 'a t
 
 (* Construction *)
 
+(** Convert a Dlist to an OCaml list. O(N), where N is the number of elements. *)
 val to_list : 'a t -> 'a list
+
+(** Convert an OCaml list to a Dlist. O(N). *)
 val of_list : 'a list -> 'a t
+
+(** Convert a Dlist to a Dlist by reconstruction. O(N) (strict evaluation). *)
 val of_dlist : 'a t -> 'a t
+
+(** Return an empty Dlist. O(1). *)
 val empty : unit -> 'a t
+
+(** Concatenate two Dlists. O(1). *)
 val append : 'a t -> 'a t -> 'a t
+
+(** Concatenate a list of Dlists. O(M), where M is the number of Dlists. *)
 val concat : 'a t list -> 'a t
 
 (* Basic functions *)
 
+(** Reverse a Dlist.
+	O(1) (lazy evaluation). *)
 val rev : 'a t -> 'a t
+
+(** Return the first element of the given list.
+	O(N). *)
 val hd : 'a t -> 'a option
+
+(** Return the first element of the given list.
+	Raise Failure "hd" if the list is empty.
+	O(N). *)
 val hd_exn : 'a t -> 'a
+
+(** Return the given list without its first element.
+	O(N). *)
 val tl : 'a t -> 'a t option
+
+(** Return the given list without its first element.
+	Raise Failure "tl" if the list is empty.
+	O(N). *)
 val tl_exn : 'a t -> 'a t
+
+(** Return the n-th element of the given list.
+	The first element (head of the list) is at position 0.
+	O(N). *)
 val nth : 'a t -> int -> 'a option
+
+(** Return the n-th element of the given list.
+	The first element (head of the list) is at position 0.
+	Raise Failure "nth" if the list is too short.
+	Raise Invalid_argument "List.nth" if n is negative.
+	O(N). *)
 val nth_exn : 'a t -> int -> 'a
 
 (* Reduction *)
 
+(** Fold the Dlist. O(N). *)
 val fold : 'a t -> init:'acc -> f:('acc -> 'a -> 'acc) -> 'acc
+
+(** Fold the Dlist from left. O(N). *)
 val fold_left : 'a t -> init:'acc -> f:('acc -> 'a -> 'acc) -> 'acc
+
+(** Fold the Dlist from right. O(N). *)
 val fold_right : 'a t -> init:'acc -> f:('a -> 'acc -> 'acc) -> 'acc
+
+(** Fold the Dlist with index. O(N). *)
 val foldi : 'a t -> init:'acc -> f:(int -> 'acc -> 'a -> 'acc) -> 'acc
+
+(** Map the Dlist. O(N). *)
 val map : 'a t -> f:('a -> 'b) -> 'b t
+
+(** Map the Dlist with index. O(N). *)
 val mapi : 'a t -> f:(int -> 'a -> 'b) -> 'b t
+
+(** Iterate the Dlist. O(N). *)
 val iter : 'a t -> f:('a -> unit) -> unit
+
+(** Iterate the Dlist with index. O(N). *)
 val iteri : 'a t -> f:(int -> 'a -> unit) -> unit
